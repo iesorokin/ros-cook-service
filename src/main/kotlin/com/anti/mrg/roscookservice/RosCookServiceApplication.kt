53252624d2a49ad6b14f7a76eeea46f339d5af56
Ivan <Ivan.Sorokin@leroymerlin.ru>
2018-11-10 21:22:14 +0300

    first init

---
+++ src/main/kotlin/com/anti/mrg/roscookservice/RosCookServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.anti.mrg.roscookservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RosCookServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RosCookServiceApplication>(*args)
+}
